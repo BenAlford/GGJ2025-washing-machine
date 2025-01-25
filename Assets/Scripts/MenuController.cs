3fed0375347765411508ec88df9fffa66105fcdf
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2025-01-25 20:08:20 +0000

    added title screen intro and loop

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] VideoPlayer intro_video_player;
+    [SerializeField] VideoPlayer outro_video_player;
+    [SerializeField] VideoPlayer loop_video_player;
+    [SerializeField] VideoPlayer cat_fade_in_video_player;
+
+    private void Start()
+    {
+        intro_video_player.loopPointReached += introFinished;
+    }
+
     public void startGame()
     {
         SceneManager.LoadScene("timertest");
@@ -13,5 +24,18 @@ public class MenuController : MonoBehaviour
     public void exitGame()
     {
         Application.Quit();
+    }
+
+    private void Update()
+    {
+    }
+
+    void introFinished(VideoPlayer video_player)
+    {
+        //stop intro, start loop
+        intro_video_player.gameObject.SetActive(false);
+
+        loop_video_player.gameObject.SetActive(true);
+        loop_video_player.Play();
     }
 }
